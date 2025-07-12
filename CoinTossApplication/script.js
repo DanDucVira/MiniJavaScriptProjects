f2aeb8ab36284ccd19c6606e562e0fc544bc9206
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2025-07-12 12:38:36 -0400

    added game logic to the project

--- CoinTossApplication/script.js
+++ CoinTossApplication/script.js
@@ -1,1 +1,13 @@
-console.log("connection works");
+const message = document.querySelector(".message");
+const btns = document.querySelectorAll("button")
+const coinArray = ["Heads", "Tails"];
+
+for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener("click", tossCoin);
+}
+
+function tossCoin(e){
+    console.log("Player: " + e.target.innerText);
+    let computerToss = Math.floor(Math.random() * 2);
+    console.log("Computer:" + coinArray[computerToss]);
+}
