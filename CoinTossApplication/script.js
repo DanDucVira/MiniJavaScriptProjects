2abcf8e6ef933d39e385d03e2f2d0b91ee60479d
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2025-07-12 14:34:29 -0400

    project completed

--- CoinTossApplication/script.js
+++ CoinTossApplication/script.js
@@ -1,13 +1,31 @@
 const message = document.querySelector(".message");
-const btns = document.querySelectorAll("button")
+const btns = document.querySelectorAll("button");
 const coinArray = ["Heads", "Tails"];
+let output;
+let score = [0, 0];
 
-for(let i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click", tossCoin);
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", tossCoin);
 }
 
-function tossCoin(e){
-    console.log("Player: " + e.target.innerText);
-    let computerToss = Math.floor(Math.random() * 2);
-    console.log("Computer:" + coinArray[computerToss]);
+function tossCoin(e) {
+  let playerGuess = e.target.innerText;
+  console.log(playerGuess);
+  let computerToss = Math.floor(Math.random() * 2);
+
+  let computerGuess = coinArray[computerToss];
+  message.innerHTML = `Computer Selected  ${computerToss} <br>`;
+  console.log(computerGuess);
+
+  if (playerGuess === computerGuess) {
+    output = "Player Wins";
+    //win
+    score[0]++;
+  } else {
+    //loss
+    output = "Computer Wins";
+    score[1]++;
+  }
+
+  message.innerHTML = `${output} <br> Player ${score[0]} Computer ${score[1]}`;
 }
