3dcf8f6d1c465bfa960e74c9bf39037ec018f12f
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2025-07-16 19:05:42 -0400

    added a eventlistener

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -2,3 +2,8 @@ const btn = document.querySelector("button");
 const playerOne = document.querySelector("#player1");
 const playerTwo = document.querySelector("#player2");
 const output = document.querySelector("#output");
+
+
+btn.addEventListener("click", function(){
+   
+})
